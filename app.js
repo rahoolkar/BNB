4b1f505923c801e7d7f63e9584e39fa1b04249e7
rahoolkar <batman30jan2002@gmail.com>
2025-01-27 01:04:34 +0530

    boilerplate

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const mongoose = require('mongoose');
 const port = 8080;
 const Listing = require("./models/listing.js");
 const path = require("path");
-var methodOverride = require('method-override')
+var methodOverride = require('method-override');
+var engine = require('ejs-mate')
 
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
+app.engine('ejs', engine);
 
 async function main() {
     mongoose.connect('mongodb://127.0.0.1:27017/bnb');
