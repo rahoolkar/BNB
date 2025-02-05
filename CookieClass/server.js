8b2190acca2be3a17e0c9b6ea4b9a03cba7af8ca
rahoolkar <batman30jan2002@gmail.com>
2025-02-05 14:55:58 +0530

    using cookie parser package

--- CookieClass/server.js
+++ CookieClass/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+var cookieParser = require('cookie-parser')
 
-app.get("/cookie",(req,res)=>{
+
+app.use(cookieParser())
+
+app.get("/showcookie",(req,res)=>{
+    console.log(req.cookies);
+    res.send(req.cookies);
+})
+
+app.get("/sendcookie",(req,res)=>{
     res.cookie("name","rahulkar"); //sending cookie
     res.send("cookie recieved");
 })
